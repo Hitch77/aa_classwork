b93d84fe8f9b3201f09c1366a0dffbe797f7bb09
Jimmy Kuang <jimmy.kuang96@gmail.com>
2021-07-23 15:57:41 -0700

    Added up to dfs

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -9,16 +9,43 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        # debugger
-        return @parent = nil if node == nil
-        @parent = node
-        if !node.children.include?(self)
-            node.children << self
-        end
+        #debugger
+        # return @parent = nil if node == nil
+        #parent = node
+        # if !node.children.include?(self)
+        #     node.children << self
+        # end
+        # if @parent != nil
+        #     @parent.children.delete(self)
+        #     @parent = node
+        #     node.children << self
+        # end
+        # self
         if @parent != nil
             @parent.children.delete(self)
-            parent = node
+        end
+        @parent = node
+        if node != nil && !node.children.include?(self) 
             node.children << self
         end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise_error if !self.children.include?(child_node)
+        child_node.parent = nil
+    end
+    
+    def dfs(value)
+        return self if self.children.empty? && self.value == value
+        return self if self.value == value
+        self.children.each do |child_node|
+            return child_node if child_node.value == value
+            child_node.dfs(value)
+        end
+        nil
     end
 end
