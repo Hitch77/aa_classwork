2349c025117dc16974d3874b859065dafa4e05d7
Hicham Elalam <he102@scarletmail.rutgers.edu>
2021-08-02 18:02:16 -0400

    added resizing

--- W5D1/skeleton/lib/p01_int_set.rb
+++ W5D1/skeleton/lib/p01_int_set.rb
@@ -23,19 +23,32 @@ end
 
 
 class IntSet
+  
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
-
+  
   def insert(num)
+    return false if include?(num)
+    i = num % store.length
+    store[i] << num
+    true
   end
-
+  
   def remove(num)
+    return false if !include?(num)
+    i = num % store.length
+    store[i].delete(num)
+    true
   end
-
+  
   def include?(num)
+    i = num % store.length
+    store[i].include?(num)
   end
-
+  
   private
 
   def [](num)
@@ -48,7 +61,8 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
+  attr_writer :count
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -56,15 +70,31 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    i = num % num_buckets
+    store[i] << num
+    @count += 1
+    if count > num_buckets
+      resize!
+    end
+    true
   end
 
   def remove(num)
+    return false if !include?(num)
+    i = num % num_buckets
+    store[i].delete(num)
+    @count -= 1
+    true
   end
 
   def include?(num)
+    i = num % num_buckets
+    store[i].include?(num)
   end
 
   private
+  attr_writer :store
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
@@ -75,5 +105,12 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = ResizingIntSet.new(2 * num_buckets)
+    @store.each do |bucket|
+      bucket.each do |val|
+        new_store.insert(val)
+      end
+    end
+    @store = new_store.store
   end
 end
