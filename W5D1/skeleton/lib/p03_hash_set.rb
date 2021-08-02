55cd37b237f16e6e9224bceae4665ee49cedea83
Hicham Elalam <he102@scarletmail.rutgers.edu>
2021-08-02 18:46:12 -0400

    added hash_set

--- W5D1/skeleton/lib/p03_hash_set.rb
+++ W5D1/skeleton/lib/p03_hash_set.rb
@@ -1,18 +1,35 @@
+require_relative 'p02_hashing.rb'
+
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
-
+  
   def insert(key)
+    return false if include?(key)
+    i = key.hash % store.length
+    store[i] << key
+    @count += 1
+    if count > num_buckets
+        resize!
+    end
+    true
   end
 
   def include?(key)
+    i = key.hash % store.length
+    store[i].include?(key)
   end
 
   def remove(key)
+    return false if !include?(key)
+    i = key.hash % num_buckets
+    store[i].delete(key)
+    @count -= 1
+    true
   end
 
   private
@@ -26,5 +43,12 @@ class HashSet
   end
 
   def resize!
+    new_store = HashSet.new(2 * num_buckets)
+    @store.each do |bucket|
+      bucket.each do |val|
+        new_store.insert(val)
+      end
+    end
+    @store = new_store.store
   end
 end
