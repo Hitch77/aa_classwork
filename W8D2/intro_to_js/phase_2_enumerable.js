7f635997485abf0b681af5b4b68af25e6ec404dd
Hicham Elalam <he102@scarletmail.rutgers.edu>
2021-08-24 20:00:11 -0400

    my inject

--- W8D2/intro_to_js/phase_2_enumerable.js
+++ W8D2/intro_to_js/phase_2_enumerable.js
@@ -17,3 +17,22 @@ let newArray = [1, 2, 3];
 const func = function(num) {
   return num * 2;
 }
+
+Array.prototype.myReduce = function (callback, initialValue = null) {
+    // if (!initialValue) {
+    //     let initialValue = this[0]
+    // }
+    if (initialValue === null){
+    initialValue = this.shift()}
+    this.myEach((ele) => {
+        initialValue = callback(ele, initialValue)
+    })
+    return initialValue;
+}
+
+array = [1, 2, 3]
+// without initialValue
+array.myReduce(function (acc, el) {return acc + el;}); // => 6
+
+// with initialValue
+array.myReduce(function (acc, el) {return acc + el;}, 25); // => 31
