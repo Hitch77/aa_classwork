fa3c0246a03c58264904c2d78ff60390b0b53fa9
Hicham <>
2021-09-21 18:16:07 -0400

    still setting up

---
+++ W12D2/benchbnb/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::SessionsController < ApplicationController
+  def create
+    # Find user by credentials
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ['Nope. Wrong credentials!'], status: 401
+    else
+      login!(@user)
+      render 'api/users/show';
+    end
+  end
+
+  def destroy
+    if session[:session_token] == nil 
+        render json: @user.errors.full_messages, status: 404
+    else 
+        logout!
+        render json: { }
+    end
+end
